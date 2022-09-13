2753653e0cb9bee9bb50676fa11558a74fd4d8b8
Alyssa Jones <97263932+atechjourney@users.noreply.github.com>
2022-09-13 07:49:28 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -67,7 +67,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-    let apiKey = "d1a86552de255334f6117b348c4519bd";
+    let apiKey = "2ff29bed3181c3526c35cc5408037f85";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 }
@@ -98,7 +98,7 @@ function displayTemperature(response) {
     getForecast(response.data.coord);
 }
 function search(city) {
-    let apikey = "d1a86552de255334f6117b348c4519bd";
+    let apiKey = "2ff29bed3181c3526c35cc5408037f85";
     let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiurl).then(displayTemperature)
 }
